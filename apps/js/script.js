d9264faeca90e35c9e1d6b0585f2de8fd6fea54b
Edward0120 <edwardrubengelegino@gmail.com>
2021-12-09 21:04:52 +0800

    added speech synthesis

--- apps/js/script.js
+++ apps/js/script.js
@@ -1,6 +1,10 @@
 const quoteText = document.querySelector(".quote"),
 quoteName = document.querySelector(".name"),
-quoteBtn = document.querySelector("button");
+quoteBtn = document.querySelector("button"),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter");
+
 
 function randomQuote(){
     fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
@@ -10,5 +14,11 @@ function randomQuote(){
 
     });
 }
+
+soundBtn.addEventListener("click", ()=>{
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${quoteName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+
 
 quoteBtn.addEventListener("click", randomQuote);
